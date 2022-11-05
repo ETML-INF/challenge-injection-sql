791ece97e33484fd8a95e070feba416b53214e7c
unknown <gregory.charmier@gmail.com>
2022-11-05 16:09:11 +0100

    Injection SQL

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
 
 $users = [];
+$isUserConnected = 0;
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
+    // Récupération du login et password saisi par l'utilisateur
     $useLogin = $_POST["useLogin"];
     $userPassword = $_POST["usePassword"];
 
-    $connector = new PDO('mysql:host=localhost;dbname=db_example_injection;charset=utf8', 'dbNicknameUser', '.ETML151');
-    $query = "SELECT * FROM t_user where useLogin = '" . $useLogin . "'";
+    // Création d'une instance PDO pour se connecter à la base de données en base de données
+    $connector = new PDO("mysql:host=localhost;dbname=challengeinjectionsql;charset=utf8", 'root', '');
 
+    // Création de la requête SQL permettant de récupérer les informations de l'utilisateur
+    // à partir du login et password saisis par l'utilisateur
+    $query = "SELECT * FROM t_user where useLogin = '" . $useLogin . "' and usePassword = '" . $userPassword . "'";
+
+    // Exécution de la requête. A noter l'utilisation de la méthode ->query()
     $req = $connector->query($query);
-    $users = $req->fetchALL(PDO::FETCH_ASSOC);
+
+    // On convertit le résultat de la requête en tableau
+    $user = $req->fetchALL(PDO::FETCH_ASSOC);
+    // Si le tableau 'user' n'est pas vide, cela signifie que l'utilisateur a bien été trouvé en DB
+    if ($user) {
+        $isUserConnected = 1;
+    }
 }
 
 ?>
@@ -24,38 +37,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link href="style.css" rel="stylesheet" />
     <title>Exemple d'injection SQL</title>
 </head>
 
 <body>
-    <h1>Exemple d'injection SQL</h1>
-
-    <form method="post" action="index.php" style="margin-top: 40px">
-        Username: <input type="text" name="useLogin" value="">
-        Password: <input type="text" name="usePassword" value="">
-        <input type="submit" name="login" value="Login">
-    </form>
-
-    <table class="table" style="margin-top: 40px">
-        <tr>
-            <td>Username</td>
-            <td>Password</td>
-            <td>Role</td>
-        </tr>
-
-        <?php
-        foreach ($users as $user) {
-            $html = "<tr>";
-            $html .= "<td>" . $user["useLogin"] . "</td>";
-            $html .= "<td>" . $user["usePassword"] . "</td>";
-            $html .= "<td>" . $user["useAdministrator"] . "</td>";
-            $html .= "</tr>";
-            echo $html;
-        }
-        ?>
-    </table>
-
+    <div class="container">
+        <h1>Exemple d'injection SQL</h1>
+        <div class="block">
+            <h2>Se connecter</h2>
+            <form method="post" action="index.php" style="margin-top: 40px">
+                Username: <input type="text" name="useLogin" value="">
+                Password: <input type="text" name="usePassword" value="">
+                <input type="submit" name="login" value="Login">
+            </form>
+        </div>
+        <div class="block">
+            <h2>Utilisateur connecté ? : <?php echo $isUserConnected ?></h2>
+            <?php if ($isUserConnected) {
+                echo "<h2>Bonjour " . $user[0]["useLogin"] . " </h2>";
+            } ?>
+        </div>
+        <div class="block">
+            <h2>Requête SQL:</h2>
+            <?php var_dump($query); ?>
+        </div>
+    </div>
 </body>
 
 </html>
